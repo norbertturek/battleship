fa09129047f5a086173e97e11bcec92996ce82c1
n.turek <n.turek@nowaera.pl>
2019-08-16 07:39:28 +0200

    374 page battleship

--- battleship.js
+++ battleship.js
@@ -32,3 +32,68 @@
 
 
 // NEW VERSION OF GAME
+
+var view = {
+
+    displayMessage: function (msg) {
+        var messageArea = document.getElementById('messageArea');
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function (location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute('class', 'hit');
+    },
+    displayMiss: function (location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute('class', 'miss');
+    }
+};
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+    ships: [
+        { locations: ['06', '16', '26'], hits: ['', '', ''] },
+        { locations: ['24', '34', '44'], hits: ['', '', ''] },
+        { locations: ['10', '11', '12'], hits: ['', '', ''] },
+    ],
+    fire: function (guess) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            var index = ship.locations.indexOf(guess);
+            // var locations = ship.locations;
+            // var index = locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = 'hit';
+                view.displayHit(guess);
+                view.displayMessage('Trafiony!');
+                if (this.isSunk(ship)) {
+                    view.displayMessage('Zatopiłeś okręt gnoju!');
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage('Pudło!');
+        return false;
+    },
+    isSunk: function (ship) {
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== 'hit') {
+                return false;
+            }
+            return true;
+        }
+    }
+};
+
+// view.displayMiss('00');
+// view.displayHit('01');
+// view.displayMessage('Co Ty kurwa chcesz od ziomka');
+
+model.fire('15');
+model.fire('00');
+model.fire('44');
